f28fb1f4b364a26108d3ed21a32e5d30c7bc9fb4
Roman <69187660+chieftainroman@users.noreply.github.com>
2022-08-07 17:15:23 +0400

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('product/<slug:slug>/', views.detail, name="product-detail"),
     path('products/category/<slug:slug>/', views.category_products, name="category-products"),
     path('search',views.search,name = "search"),
-
+    path('payment_error',views.payment_error,name = "payment_error"),
+    path('payment_success',views.payment_success,name = "payment_success"),
+    path('payment_alerts',views.payment_alerts,name = "payment_alerts"),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
