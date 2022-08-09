9fb908652ed0b4fdbb9901640f6053f422f928a8
Roman <69187660+chieftainroman@users.noreply.github.com>
2022-08-09 23:32:42 +0400

    Update product_tags.py

--- pages/templatetags/product_tags.py
+++ pages/templatetags/product_tags.py
@@ -7,5 +7,5 @@ def call_sellprice(product_price,discount_percent ):
     if discount_percent == None or discount_percent == 0:
         return product_price
     sellprice = product_price
-    sellprice = product_price - (product_price * discount_percent /100)
+    sellprice = round(product_price - (product_price * discount_percent /100))
     return sellprice
