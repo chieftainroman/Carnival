34d1a130be942b702ba65a00e1888c143e1e068b
Roman <69187660+chieftainroman@users.noreply.github.com>
2022-09-11 02:00:18 +0400

    Update forms.py

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,45 @@
 from django import forms
 from .models import Contact
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.models import User  
+
+class SignupForm(UserCreationForm):  
+    email = forms.EmailField(max_length=200, help_text='Required')  
+    class Meta:  
+        model = User  
+        fields = ('username', 'email', 'password1', 'password2')  
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Пользователь с таким username уже существует")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Пользователь с таким email уже существует")
+        return email
+    
+        
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Ваш Username'
+        })
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'Ваш Email'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Пароль'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Повторите пароль'
+        })
+
+
 
 
 class ContactForm(forms.Form):
