dc88e37316a6fd11350347fbd50507ac8276de42
Roman <69187660+chieftainroman@users.noreply.github.com>
2022-09-11 13:25:58 +0400

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -89,7 +89,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):  
         user.is_active = True  
         user.save()  
-        return render(request,"account_activated.html")
+        return redirect("login")
     else:  
         return HttpResponse('Activation link is invalid!')  
 
