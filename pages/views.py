31b44fa97bc5d15597888c6034a2cb65c2520534
Roman <69187660+chieftainroman@users.noreply.github.com>
2022-09-12 20:46:06 +0400

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from itertools import product
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from . models import Offers, Slider, Products, SaleOffers, ProductsImage, Category, Cart
 from django.shortcuts import get_object_or_404
-from . forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 from django.conf import settings
@@ -49,7 +49,6 @@ def index(request):
 
     return render(request, "index.html", context)
 
-
 def signup(request): 
     if request.user.is_authenticated:
         return redirect("home")
@@ -61,44 +60,13 @@ def signup(request):
             user = form.save(commit=False)  
             user.is_active = False  
             user.save()  
-            # to get the domain of the current site  
-            current_site = get_current_site(request)  
-            mail_subject = 'Activation link has been sent to your email id'  
-            message = render_to_string('authentication/acc_active_email.html', {  
-                'user': user,  
-                'domain': current_site.domain,  
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
-                'token':account_activation_token.make_token(user),  
-            })  
-            to_email = form.cleaned_data.get('email')  
-            email = EmailMessage(  
-                        mail_subject, message, to=[to_email]  
-            )  
-            email.send()  
-            return HttpResponse('Please confirm your email address to complete the registration')  
+            return redirect("login")
     else:  
         form = SignupForm()  
     return render(request, 'authentication/signup.html', {'form': form})  
 
-def activate(request, uidb64, token):  
-    User = get_user_model()  
-    try:  
-        uid = force_str(urlsafe_base64_decode(uidb64))  
-        user = User.objects.get(pk=uid)  
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
-        user = None  
-    if user is not None and account_activation_token.check_token(user, token):  
-        user.is_active = True  
-        user.save()  
-        return redirect("login")
-    else:  
-        return HttpResponse('Activation link is invalid!')  
-
 def Login(request):
-    if request.method == 'POST':
-      
-        # AuthenticationForm_can_also_be_used__
-  
+    if request.method == 'POST':  
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username = username, password = password)
@@ -110,6 +78,10 @@ def Login(request):
             messages.info(request, f'account done not exit plz sign in')
     form = AuthenticationForm()
     return render(request, 'authentication/login.html', {'form':form, 'title':'log in'})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
 
 def search(request):
     categories = Category.objects.all()
@@ -125,7 +97,6 @@ def search(request):
     }
     return render(request,"products.html",context)
 
-
 def products(request):
     products = Products.objects.all()
     first_topic_categories = Category.objects.filter(first_in_the_topic=True)
@@ -137,8 +108,6 @@ def products(request):
     }
 
     return render(request, "products.html", context)
-
-
 
 def detail(request, slug):
     product = get_object_or_404(Products, slug=slug)
@@ -171,9 +140,9 @@ def add_to_cart(request):
 
 @login_required(login_url="https://www.carnival-shop.info/login/")
 def cart(request):
-
     user = request.user
     cart_products = Cart.objects.filter(user=user)
+
     # Display Total on Cart Page
     amount = decimal.Decimal(0)
     shipping_amount = decimal.Decimal(10)
@@ -234,35 +203,3 @@ def category_products(request, slug):
 
     }
     return render(request, 'category_products.html', context)
-
-def payment_alerts(request):
-    return render(request, 'payment_alerts.html')
-
-        
- 
-#При успешной оплате
-def payment_success(request):
-    return render(request, 'success_payment.html')
- 
- 
-#При ошибке в оплате
-def payment_error(request):
-    return render(request, 'error_payment.html')
-
-def balance(request):
-    merchant_id = '20856'
-    secret_word = ',?>GmGGS(?&UEBx'
-    order_id = '154'
-    order_amount = '10.11'
-    currency = 'RUB'
-    sign = hashlib.md5(f'{merchant_id}:{order_amount}:{secret_word}:{currency}:{order_id}'.encode('utf-8')).hexdigest()
- 
-    context = {
-        'm': merchant_id,
-        'oa': order_amount,
-        'o': order_id,
-        's': sign,
-        'currency': currency
-    }
- 
-    return render(request, 'balance.html', context)
