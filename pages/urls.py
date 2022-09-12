039e16d4447802916a02c36be6ad3300a2f96a1a
Roman <69187660+chieftainroman@users.noreply.github.com>
2022-09-12 20:45:43 +0400

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -9,10 +9,6 @@ urlpatterns = [
     path('product/<slug:slug>/', views.detail, name="product-detail"),
     path('products/category/<slug:slug>/', views.category_products, name="category-products"),
     path('search',views.search,name = "search"),
-    path('payment_error',views.payment_error,name = "payment_error"),
-    path('payment_success',views.payment_success,name = "payment_success"),
-    path('payment_alerts',views.payment_alerts,name = "payment_alerts"),
-    path('payment',views.balance,name = "balance"),
     path('cart/',views.cart,name = "cart"),
     path('add-to-cart/', views.add_to_cart, name="add-to-cart"),
     path('remove-cart/<int:cart_id>/', views.remove_cart, name="remove-cart"),
@@ -20,12 +16,12 @@ urlpatterns = [
     path('minus-cart/<int:cart_id>/', views.minus_cart, name="minus-cart"),
     path('signup/',views.signup,name = "signup"),
     path('login/',views.Login,name = "login"),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',  views.activate, name='activate'),
+    path('logout/',views.logout_view,name = "logout"),
     
-    #path("reset_password/", auth_views.PasswordResetView.as_view(),name="reset_password"),
-    #path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
-    #path("reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    #path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"), 
+    path("reset_password/", auth_views.PasswordResetView.as_view(),name="reset_password"),
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path("reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"), 
 
 
 
