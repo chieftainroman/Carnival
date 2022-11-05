5223789f05258c5c044407d3fd5b478fdfa9f7fc
Chieftainroma <romanmammadov872@gmail.com>
2022-11-05 22:21:30 +0400

    my changes

--- fashion/settings.py
+++ fashion/settings.py
@@ -144,7 +144,7 @@ DEFAULT_FROM_EMAIL = 'carnivalshopru@gmail.com'
 STATIC_URL = 'static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static/")]
-STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
